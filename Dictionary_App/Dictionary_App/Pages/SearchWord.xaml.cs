56fd5c894ddd737b932a777cb7413e43095576a6
Raluca <spatarularisaraluca@gmail.com>
2024-03-17 04:31:00 +0200

    Add search module

--- Dictionary_App/Dictionary_App/Pages/SearchWord.xaml.cs
+++ Dictionary_App/Dictionary_App/Pages/SearchWord.xaml.cs
@@ -1,28 +1,73 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Dictionary_App.MyApp;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace Dictionary_App.Pages
 {
-    /// <summary>
-    /// Interaction logic for SearchWord.xaml
-    /// </summary>
+
     public partial class SearchWord : Page
     {
         public SearchWord()
         {
             InitializeComponent();
+            InitializeComboBox();
+
+        }
+
+        private void InitializeComboBox()
+        {
+            categoryComboBox.ItemsSource = MyDictionary._categories;
+        }
+
+        private void categoryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string selectedCategory = categoryComboBox.SelectedItem.ToString();
+            if (selectedCategory != null && selectedCategory != "All")
+            {
+                MyDictionary.GetSuggestedWords(selectedCategory);
+
+
+                if (MyDictionary._suggestedWords.Any())
+                {
+                    suggestionsListBox.Items.Clear();
+                    foreach (var word in MyDictionary._suggestedWords)
+                    {
+                        suggestionsListBox.Items.Add(word._name);
+                    }
+                }
+                else
+                {
+                    txtSearch.Text.Remove(0);
+                }
+            }
+            suggestionsListBox.Visibility = Visibility.Visible;
+        }
+
+        private void txtSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            suggestionsListBox.Visibility = Visibility.Visible;
+            string searchText = txtSearch.Text.ToLower();
+            var filteredWords = MyDictionary._suggestedWords.Where(word => word._name.ToLower().StartsWith(searchText)).ToList();
+            suggestionsListBox.Items.Clear();
+            foreach (var word in filteredWords)
+            {
+                suggestionsListBox.Items.Add(word._name);
+            }
+        }
+
+        private void suggestionsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            suggestionsListBox.Visibility = Visibility.Collapsed;
+            if (suggestionsListBox.SelectedItem != null)
+                MyDictionary._selectedWord = MyDictionary._suggestedWords.Where(word => word._name == suggestionsListBox.SelectedItem.ToString()).FirstOrDefault();
+            imgWord.Source = new BitmapImage(new Uri(MyDictionary._selectedWord._imagePath, UriKind.Relative));
+            txtDescription.Text = MyDictionary._selectedWord._definition;
+            txtName.Text = MyDictionary._selectedWord._name;
         }
     }
 }
