797571043f8cee5c2a8f6540eef1693ca92eddbf
Raluca <spatarularisaraluca@gmail.com>
2024-03-19 10:56:00 +0200

    Add the game logics

---
+++ Dictionary_App/Dictionary_App/MyApp/GameLogic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionary_App.MyApp
+{
+    static class GameLogic
+    {
+        static public List<Word> _wordsToGuess= new List<Word>();
+        static public int _score;
+        static public int _currentRound;
+
+        static GameLogic()
+        {
+            SelectWords();
+        }
+
+        //select 5 words randomly from the list of words
+        public static void SelectWords()
+        {
+            Random random = new Random();
+            List<Word> allWords = MyDictionary._words;
+            for (int i = 0; i < 5; i++)
+            {
+                int index = random.Next(0, allWords.Count);
+                _wordsToGuess.Add(allWords[index]);
+                allWords.RemoveAt(index);
+            }
+        }
+    }
+}
--- Dictionary_App/Dictionary_App/Pages/Game.xaml.cs
+++ Dictionary_App/Dictionary_App/Pages/Game.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Dictionary_App.MyApp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +17,77 @@ using System.Windows.Shapes;
 
 namespace Dictionary_App.Pages
 {
-    /// <summary>
-    /// Interaction logic for Game.xaml
-    /// </summary>
     public partial class Game : Page
     {
         public Game()
         {
             InitializeComponent();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            GameLogic._currentRound = 1;
+            GameLogic._score = 0;
+            ShowNextWord();
+        }
+
+        private void ShowNextWord()
+        {
+            if (GameLogic._currentRound <= 5)
+            {
+                if (GameLogic._wordsToGuess[GameLogic._currentRound - 1]._imagePath == "..\\Images\\no_image.jpg")
+                {
+                    txtDescription.Visibility = Visibility.Visible;
+                    border.Visibility = Visibility.Visible;
+                    imgWord.Visibility = Visibility.Hidden;
+                    txtDescription.Text = GameLogic._wordsToGuess[GameLogic._currentRound - 1]._definition;
+                }
+                else
+                {
+                    imgWord.Visibility = Visibility.Visible;
+                    border.Visibility = Visibility.Hidden;
+                    txtDescription.Visibility = Visibility.Hidden;
+                    imgWord.Source = new BitmapImage(new Uri(GameLogic._wordsToGuess[GameLogic._currentRound - 1]._imagePath, UriKind.Relative));
+                }
+                countRounds.Text = $"Word {GameLogic._currentRound}/5";
+                txtGuess.Text = "";
+            }
+            else
+            {
+                txtDescription.Visibility = Visibility.Visible;
+                txtGuess.Visibility = Visibility.Hidden;
+                countRounds.Visibility = Visibility.Hidden;
+                imgWord.Visibility = Visibility.Hidden;
+                btnExit.Visibility = Visibility.Visible;
+                txtDescription.Text = $"Your score is {GameLogic._score}/5";
+            }
+        }
+
+        private void CheckGuess()
+        {
+            if (txtGuess.Text.Equals(GameLogic._wordsToGuess[GameLogic._currentRound - 1]._name, StringComparison.OrdinalIgnoreCase))
+            {
+                GameLogic._score++;
+                // Display a message that the guess is correct
+
+            }
+            GameLogic._currentRound++;
+            ShowNextWord();
+        }
+
+        private void txtGuess_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                CheckGuess();
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Uri("Pages/Landing.xaml", UriKind.Relative));
         }
     }
+
 }
